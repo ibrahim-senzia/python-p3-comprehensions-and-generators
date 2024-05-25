52151ef73e19176df8538a7c8da25c23b6d28a5a
Ibrahim Senzia <ibrahimsenzia@gmail.com>
2024-05-25 10:39:33 +0300

    list generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [n for n in num_list if n % 2 ==0]
+    if not evens:
+        return []
+    return evens
+
+return_evens([1,2,3,4,5,6,7,8])
 
 def make_exclamation(sentence_list):
-    pass
+    updated_list = [char + ('!') for char in sentence_list]
+    if not updated_list:
+        return []
+    return updated_list
+
+make_exclamation(['Hello', 'Marrie'])
+
